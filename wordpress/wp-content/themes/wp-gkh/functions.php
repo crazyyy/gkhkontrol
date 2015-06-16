d94c49285f43e8fda39fcddf9f17921f916f1aef
crazyyy <crazyyy@gmail.com>
2015-06-17 00:24:31 +0300

    fix rewrite rules

--- wordpress/wp-content/themes/wp-gkh/functions.php
+++ wordpress/wp-content/themes/wp-gkh/functions.php
@@ -489,29 +489,6 @@ add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10); // Remove
 // Remove Filters
 remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from Excerpt altogether
 
-// Shortcodes
-add_shortcode('html5_shortcode_demo', 'html5_shortcode_demo'); // You can place [html5_shortcode_demo] in Pages, Posts now.
-add_shortcode('html5_shortcode_demo_2', 'html5_shortcode_demo_2'); // Place [html5_shortcode_demo_2] in Pages, Posts now.
-
-// Shortcodes above would be nested like this -
-// [html5_shortcode_demo] [html5_shortcode_demo_2] Here's the page title! [/html5_shortcode_demo_2] [/html5_shortcode_demo]
-/*------------------------------------*\
-    ShortCode Functions
-\*------------------------------------*/
-
-// Shortcode Demo with Nested Capability
-function html5_shortcode_demo($atts, $content = null)
-{
-    return '<div class="shortcode-demo">' . do_shortcode($content) . '</div>'; // do_shortcode allows for nested Shortcodes
-}
-
-// Shortcode Demo with simple <h2> tag
-function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 shortcode, allows for nesting within above element. Fully expandable.
-{
-    return '<h2>' . $content . '</h2>';
-}
-
-
 /*
  * ========================================================================
  *  Дополнительно всякий мусор
@@ -702,81 +679,131 @@ Author URI: http://fortes.com/
 */
 
 // In case we're running standalone, for some odd reason
-if (function_exists('add_action'))
-{
-    register_activation_hook(__FILE__, 'top_level_cats_activate');
-    register_deactivation_hook(__FILE__, 'top_level_cats_deactivate');
+// if (function_exists('add_action'))
+// {
+//     register_activation_hook(__FILE__, 'top_level_cats_activate');
+//     register_deactivation_hook(__FILE__, 'top_level_cats_deactivate');
 
-    // Setup filters
-    add_filter('category_rewrite_rules', 'top_level_cats_category_rewrite_rules');
-    add_filter('generate_rewrite_rules', 'top_level_cats_generate_rewrite_rules');
-    add_filter('category_link', 'top_level_cats_category_link', 10, 2);
+//     // Setup filters
+//     add_filter('category_rewrite_rules', 'top_level_cats_category_rewrite_rules');
+//     add_filter('generate_rewrite_rules', 'top_level_cats_generate_rewrite_rules');
+//     add_filter('category_link', 'top_level_cats_category_link', 10, 2);
 
-    global $clean_category_rewrites, $clean_rewrites;
-    $clean_category_rewrites = array();
+//     global $clean_category_rewrites, $clean_rewrites;
+//     $clean_category_rewrites = array();
+// }
+
+// function top_level_cats_activate()
+// {
+//     global $wp_rewrite;
+//     $wp_rewrite->flush_rules();
+// }
+
+// function top_level_cats_deactivate()
+// {
+//     // Remove the filters so we don't regenerate the wrong rules when we flush
+//     remove_filter('category_rewrite_rules', 'top_level_cats_category_rewrite_rules');
+//     remove_filter('generate_rewrite_rules', 'top_level_cats_generate_rewrite_rules');
+//     remove_filter('category_link', 'top_level_cats_category_link');
+
+//     global $wp_rewrite;
+//     $wp_rewrite->flush_rules();
+// }
+
+// function top_level_cats_generate_rewrite_rules($wp_rewrite)
+// {
+//     global $clean_category_rewrites;
+//     $wp_rewrite->rules = $wp_rewrite->rules + $clean_category_rewrites;
+// }
+
+// function top_level_cats_category_rewrite_rules($category_rewrite)
+// {
+//     global $clean_category_rewrites;
+
+//   global $wp_rewrite;
+//   // Make sure to use verbose rules, otherwise we'll clobber our
+//   // category permalinks with page permalinks
+//   $wp_rewrite->use_verbose_page_rules = true;
+
+//     while (list($k, $v) = each($category_rewrite)) {
+//         // Strip off the category prefix
+//         $new_k = top_level_cats_remove_cat_base($k);
+//         $clean_category_rewrites[$new_k] = $v;
+//     }
+
+//     return $category_rewrite;
+// }
+
+// function top_level_cats_category_link($cat_link, $cat_id)
+// {
+//     return top_level_cats_remove_cat_base($cat_link);
+// }
+
+// function top_level_cats_remove_cat_base($link)
+// {
+//     $category_base = get_option('category_base');
+
+//     // WP uses "category/" as the default
+//     if ($category_base == '')
+//         $category_base = 'category';
+
+//     // Remove initial slash, if there is one (we remove the trailing slash in the regex replacement and don't want to end up short a slash)
+//     if (substr($category_base, 0, 1) == '/')
+//         $category_base = substr($category_base, 1);
+
+//     $category_base .= '/';
+
+//     return preg_replace('|' . $category_base . '|', '', $link, 1);
+// }
+
+
+add_action( 'init', 'cptui_register_my_cpts' );
+function cptui_register_my_cpts() {
+    $labels = array(
+        "name" => "Ардатовский район",
+        "singular_name" => "Ардатовский район",
+        "menu_name" => "Ардатовский район",
+        "all_items" => "Ардатовский район",
+        "add_new" => "Добавить",
+        "add_new_item" => "Добавить",
+        "edit" => "Редактировать",
+        "edit_item" => "Редактировать",
+        "new_item" => "Новая",
+        "view" => "Смотреть",
+        "view_item" => "Смотреть",
+        "search_items" => "Искать",
+        "not_found" => "Не найдено",
+        "not_found_in_trash" => "Не найдено",
+        "parent" => "Родительская",
+        );
+
+    $args = array(
+        "labels" => $labels,
+        "hierarchical" => true,
+        "supports" => array( "title", "editor", "comments", "revisions", "thumbnail", "author" ),
+        "description" => "",
+        "public" => true,
+        "show_ui" => true,
+        "has_archive" => true,
+        "show_in_menu" => true,
+        "exclude_from_search" => false,
+        "capability_type" => "ardatovskij-rajon",
+        "map_meta_cap" => true,
+        'show_in_nav_menus' => true,
+        'publicly_queryable' => true,
+        // 'rewrite' => true,
+        "rewrite" => array( "slug" => "ardatovskij-rajon", "with_front" => true ),
+        "query_var" => true,
+        'can_export' => true,
+        "menu_position" => 6,
+
+    );
+    register_post_type( "ardatovskij-rajon", $args );
+
+// End of cptui_register_my_cpts()
 }
 
-function top_level_cats_activate()
-{
-    global $wp_rewrite;
-    $wp_rewrite->flush_rules();
-}
 
-function top_level_cats_deactivate()
-{
-    // Remove the filters so we don't regenerate the wrong rules when we flush
-    remove_filter('category_rewrite_rules', 'top_level_cats_category_rewrite_rules');
-    remove_filter('generate_rewrite_rules', 'top_level_cats_generate_rewrite_rules');
-    remove_filter('category_link', 'top_level_cats_category_link');
 
-    global $wp_rewrite;
-    $wp_rewrite->flush_rules();
-}
-
-function top_level_cats_generate_rewrite_rules($wp_rewrite)
-{
-    global $clean_category_rewrites;
-    $wp_rewrite->rules = $wp_rewrite->rules + $clean_category_rewrites;
-}
-
-function top_level_cats_category_rewrite_rules($category_rewrite)
-{
-    global $clean_category_rewrites;
-
-  global $wp_rewrite;
-  // Make sure to use verbose rules, otherwise we'll clobber our
-  // category permalinks with page permalinks
-  $wp_rewrite->use_verbose_page_rules = true;
-
-    while (list($k, $v) = each($category_rewrite)) {
-        // Strip off the category prefix
-        $new_k = top_level_cats_remove_cat_base($k);
-        $clean_category_rewrites[$new_k] = $v;
-    }
-
-    return $category_rewrite;
-}
-
-function top_level_cats_category_link($cat_link, $cat_id)
-{
-    return top_level_cats_remove_cat_base($cat_link);
-}
-
-function top_level_cats_remove_cat_base($link)
-{
-    $category_base = get_option('category_base');
-
-    // WP uses "category/" as the default
-    if ($category_base == '')
-        $category_base = 'category';
-
-    // Remove initial slash, if there is one (we remove the trailing slash in the regex replacement and don't want to end up short a slash)
-    if (substr($category_base, 0, 1) == '/')
-        $category_base = substr($category_base, 1);
-
-    $category_base .= '/';
-
-    return preg_replace('|' . $category_base . '|', '', $link, 1);
-}
 
 ?>
